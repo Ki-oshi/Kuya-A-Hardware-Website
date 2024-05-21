7b2b658610684276f26a8589f7bad3db8920bcf0
kio <165408404+Ki-oshi@users.noreply.github.com>
2024-05-21 13:55:00 +0000

    HAMBURGER MENU IMPLEMENTED TO ALL HTML PROD 8 WONT READ THE HAMBURGER DISPLAY SENDING HELP

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
     body.classList.toggle("nav-active");
-    slider.classList.toggle("slider--collapsed"); // Toggle the class to collapse or expand the slider
-});
+    slider.classList.toggle("slider--collapsed");
+    });
 
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
     hamburger.classList.remove("active");
@@ -16,7 +16,6 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
     body.classList.remove("nav-active");
     slider.classList.remove("slider--collapsed");
 
-    // Smooth scroll to the section corresponding to the clicked link
     const id = n.getAttribute("href").substring(1);
     const target = document.getElementById(id);
     target.scrollIntoView({ behavior: "smooth" });
